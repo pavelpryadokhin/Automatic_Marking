db19467a301260e061fb41eebdea116cae242d4c
pavel <mrpavel23ooo@mail.ru>
2023-10-11 08:34:12 +0300

    Первая версия

--- botclick.py
+++ botclick.py
@@ -6,14 +6,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium_stealth import stealth
 from selenium.webdriver.common.by import By
+from PIL import Image
 
 xpath_ETU = '/html/body/div/div/div[2]/div/div/div[2]/p/div/button'
 xpath_login = '/html/body/div/div[1]/div/div[2]/div/form/div[1]/div/div/input'
 xpath_password = '/html/body/div/div[1]/div/div[2]/div/form/div[2]/div/div/input'
 xpath_IN = '/html/body/div/div[1]/div/div[2]/div/form/div[4]/div[2]/div/button'
 xpath_authorization = '/html/body/div/div[1]/div/div[2]/div/div[4]/div/div[2]/form/button'
-# xpath_mark = '/html/body/div/div/div[3]/div/div/div/div[2]/div[1]/div/div/div/div[1]/div[1]/div'
 xpath_mark = '/html/body/div/div/div[3]/div/div/div/div[2]/div[1]/div/div/div/div[1]/div/div/button'
+
 
 def located(browser, xpath):
     return WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.XPATH, xpath)))
@@ -32,6 +33,7 @@ def mark_etu(login, password):
     stealth(browser, languages=['en-US', 'en'], vendor='Google Inc.', platform='Win32', webgl_vendor='Intel Inc.',
             renderer='Intel Iris OpenGl Engine', fix_hairline=True)
     flag = True
+    screenshot = Image.open('error.png')
     try:
         browser.get('https://digital.etu.ru/attendance/auth')
         located(browser, xpath_ETU).click()
@@ -39,11 +41,19 @@ def mark_etu(login, password):
         browser.find_element('xpath', xpath_password).send_keys(password)
         located(browser, xpath_IN).click()
         located(browser, xpath_authorization).click()
-        browser.find_element('xpath', xpath_authorization).click()
         located(browser, xpath_mark).click()
-        browser.save_screenshot('mark.png')
+        screenshot = browser.find_element('xpath', xpath_mark).screenshot('./successful.png')
+        screenshot = Image.open('successful.png')
     except Exception:
         flag = False
     finally:
         browser.quit()
-    return flag
+    return flag, screenshot
+
+
+def botclick(login, password):
+    result = mark_etu(login, password)
+    if not result[0]:
+        time.sleep(10)
+        result = mark_etu(login, password)
+    return result[1]
--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-from botclick import mark_etu
+from botclick import botclick
 import time
 
 login = 'mrpavel23ooo@mail.ru'
 password = 'v234praw'
 
 def main():
-    flag = True
-    if not mark_etu(login, password):
-        time.sleep(10)
-        flag = mark_etu(login, password)
-    if flag == True:
-        print('Выполнилось')
-    else:
-        print('Ошибка')
+    res=botclick(login, password)
 
 
 if __name__ == "__main__":
